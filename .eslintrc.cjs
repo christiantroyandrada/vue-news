9c7ef897a0e47e4fdf8dff1dbaf2f63ab9237a7f
Troy Andrada <christiantroyandrada@gmail.com>
2022-06-28 09:33:51 +0800

    eslint crlf fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,5 +7,24 @@ module.exports = {
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "@vue/eslint-config-prettier"
-  ]
+  ],
+  "rules": {
+    "endOfLine": "auto",
+    "import/no-extraneous-dependencies": "off",
+    "import/prefer-default-export": "off",
+    "no-confusing-arrow": "off",
+    "linebreak-style": "off",
+    "arrow-parens": ["error", "as-needed"],
+    "comma-dangle": [
+      "error",
+      {
+        "arrays": "always-multiline",
+        "objects": "always-multiline",
+        "imports": "always-multiline",
+        "exports": "always-multiline",
+        "functions": "ignore"
+      }
+    ],
+    "no-plusplus": "off"
+  },
 }
